d97f5c8bcdf41cb448852c2d868490ee5dc08fd4
khalifa47 <khalifafumo5@gmail.com>
2022-03-20 17:33:57 +0300

    api endpoints done for news

---
+++ src/requests/news/requests.js
@@ -0,0 +1,9 @@
+const API_KEY = "7044a24af7a64a16b66110f112087002";
+
+const requests = {
+    fetchTopHeadlines: (country = false, category = false, sources = false, q = false, language = "en") => (
+        `/top-headlines?apiKey=${API_KEY}${country && sources === false ? `&country=${country}` : ""}${category && sources === false ? `&category=${category}` : ""}${sources && country === false && category === false ? `&sources=${sources}` : ""}${q ? `&q=${q}` : ""}&language=${language}`
+    )
+};
+
+export default requests;
