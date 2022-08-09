30f19ecb1bc91e7bad2aa121d03ea7bc9b9bda36
khalifa47 <khalifafumo5@gmail.com>
2022-08-09 13:13:28 +0300

    req

--- src/requests/weather/requests.js
+++ src/requests/weather/requests.js
@@ -1,4 +1,4 @@
-const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY ? process.env.REACT_APP_WEATHER_API_KEY : "3c72c025421eb8ec553bb5f13e594c17";
 
 const requests = {
     fetchCurrent: (lat, lon) => `/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
